39974c421fcd63afa8265f98dcace769b5393470
sayuki2117 <fastrackforever@gmail.com>
2022-10-20 21:37:31 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,21 @@
 #include "lists.h"
-#include <stdio.h>
-
 /**
- * print_list - prints all the elements of a list_t list
- * @k: pointer to the start of the list
- *
- * Return: number of nodes in the list
- */
-size_t print_list(const list_t *k)
+  * print_list - a function that print all strings from a structure
+  *@h : the head of the struct
+  *Return: the length of the string
+  */
+
+size_t print_list(const list_t *h)
 {
-	const list_t *tmp;
-	unsigned int a;
-
-	tmp = k;
-	for (a = 0; tmp; a++)
-	{
-		printf("[%u] %s\n", tmp->len, tmp->str);
-		tmp = tmp->next;
-	}
-	return (a);
+size_t i = 0;
+while (h)
+{
+if (h->str != NULL)
+printf("[%u] %s\n", h->len, h->str);
+else
+printf("[0] (nil)\n");
+h = h->next;
+i++;
 }
-
+return (i);
+}
