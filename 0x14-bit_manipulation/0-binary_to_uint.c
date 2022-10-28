62f5e7253afe2885d2bd3cacf7d765b1c98f9fb0
sayuki2117 <fastrackforever@gmail.com>
2022-10-27 20:49:31 -0700

    0-binary_to_uint.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * convert_to_binary - converts a binary number to unsigned int
+ * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
  *
  * Return: the converted number
  */
-unsigned int convert_to_binary(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int dec_val = 0;
